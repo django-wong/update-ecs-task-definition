97ab94b700bdb04ef336809bbe032f96287b8783
django-wong <me@djangowong.com>
2022-09-13 21:24:49 +0800

    add option to wait for service to become stable

--- index.ts
+++ index.ts
@@ -1,65 +1,97 @@
 import * as core from "@actions/core";
 import { ECS } from "aws-sdk";
 
-const R_T = {required: true, trimWhitespace: true};
-const O_T = {required: false, trimWhitespace: true};
+const R_T = { required: true, trimWhitespace: true };
+const O_T = { required: false, trimWhitespace: true };
 
 async function _run() {
     // Required inputs
-    const family = core.getInput('task-definition-family', R_T);
-    const image = core.getInput('image', R_T);
+    const family = core.getInput("task-definition-family", R_T);
+    const image = core.getInput("image", R_T);
 
     // Optional inputs
-    const cluster = core.getInput('cluster', O_T);
-    const containerName = core.getInput('container', O_T);
-    const service = core.getInput('service', O_T);
-    const forceNewDeployment = core.getBooleanInput('force-new-deployment', O_T);
+    const cluster = core.getInput("cluster", O_T);
+    const containerName = core.getInput("container", O_T);
+    const service = core.getInput("service", O_T);
+
+    const forceNewDeployment = core.getBooleanInput(
+        "force-new-deployment",
+        O_T
+    );
+
+    const waitForService = core.getBooleanInput(
+        "wait-for-service-stability",
+        O_T
+    );
+
+    const waitForMinutes = core.getInput("wait-for-minutes", O_T);
 
     const ecs = new ECS();
-    const res = await ecs.describeTaskDefinition({taskDefinition: family}).promise();
+    const res = await ecs
+        .describeTaskDefinition({ taskDefinition: family })
+        .promise();
 
     const task = res.taskDefinition;
-    
+
     if (!task) {
-        throw new Error('Task definition not found');
+        throw new Error("Task definition not found");
     }
 
     res.taskDefinition?.containerDefinitions?.forEach((container) => {
-        if ((containerName && (container.name ?? "").match(new RegExp(containerName))) || !containerName) {
+        if (
+            (containerName &&
+                (container.name ?? "").match(new RegExp(containerName))) ||
+            !containerName
+        ) {
             container.image = image;
         }
     });
 
-    const updated = await ecs.registerTaskDefinition({
-        family: family,
-        containerDefinitions: task.containerDefinitions ?? [],
-        taskRoleArn: task.taskRoleArn,
-        executionRoleArn: task.executionRoleArn,
-        networkMode: task.networkMode,
-        volumes: task.volumes,
-        placementConstraints: task.placementConstraints,
-        requiresCompatibilities: task.requiresCompatibilities,
-        cpu: task.cpu,
-        memory: task.memory
-    }).promise();
-
+    const updated = await ecs
+        .registerTaskDefinition({
+            family: family,
+            containerDefinitions: task.containerDefinitions ?? [],
+            taskRoleArn: task.taskRoleArn,
+            executionRoleArn: task.executionRoleArn,
+            networkMode: task.networkMode,
+            volumes: task.volumes,
+            placementConstraints: task.placementConstraints,
+            requiresCompatibilities: task.requiresCompatibilities,
+            cpu: task.cpu,
+            memory: task.memory,
+        })
+        .promise();
 
     if (!updated.taskDefinition?.family) {
-        throw new Error('Can not update task definition');
+        throw new Error("Can not update task definition");
     }
 
-    core.setOutput('revision', updated.taskDefinition.revision);
+    core.setOutput("revision", updated.taskDefinition.revision);
 
     if (!service) {
         return;
     }
 
-    await ecs.updateService({
-        cluster,
-        service,
-        forceNewDeployment: forceNewDeployment,
-        taskDefinition: `${updated.taskDefinition?.family}:${updated.taskDefinition?.revision}`,
-    }).promise()
+    await ecs
+        .updateService({
+            cluster,
+            service,
+            forceNewDeployment: forceNewDeployment,
+            taskDefinition: `${updated.taskDefinition?.family}:${updated.taskDefinition?.revision}`,
+        })
+        .promise();
+
+    const minutes = parseInt(waitForMinutes || "15");
+
+    waitForService &&
+        ecs.waitFor("servicesStable", {
+            services: [service],
+            cluster: cluster,
+            $waiter: {
+                delay: 15,
+                maxAttempts: (minutes * 60) / 15,
+            },
+        });
 }
 
 _run();
